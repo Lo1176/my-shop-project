2f4b9b2a9e9cadf5ea3d98cf209362696b58233e
Laurent Binder <laurentbinder@gmail.com>
2021-03-26 11:01:14 +0100

    add private def set_shop and shop_params in controller shops and add link_to in index view

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,17 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: [:show]
+
   def index
     @shops = Shop.all
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+
+  def shop_params
+    params.require(:shop).permit(:name)
   end
 end
