f4a4d40d6e78ecfd543209c0bae27db7c8554faa
Laurent Binder <laurentbinder@gmail.com>
2021-03-26 16:01:20 +0100

    config.time_zone = Paris in application.rb and testing hours in shop Show view

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module MyShopProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # We supposed that all our shops are located in France
+    config.time_zone = 'Paris'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
